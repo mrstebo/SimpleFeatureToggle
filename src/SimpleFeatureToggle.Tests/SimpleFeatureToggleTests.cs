216de687d1b16925d9aeba28e1c21da5131e89c0
Steven Atkinson <steven.atkinson@gmx.com>
2016-10-25 16:26:05 +0100

    Added additional methods. Returning an empty collection if file cannot be read.

--- src/SimpleFeatureToggle.Tests/SimpleFeatureToggleTests.cs
+++ src/SimpleFeatureToggle.Tests/SimpleFeatureToggleTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Moq;
 using NUnit.Framework;
 
@@ -19,12 +20,12 @@ namespace SimpleFeatureToggle.Tests
         [Test]
         public void PublicConstructor_ShouldReturn_NewInstance()
         {
-            var simpleFeatureToggle = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
             {
                 Filename = "test.json"
             });
 
-            Assert.IsNotNull(simpleFeatureToggle);
+            Assert.IsNotNull(sft);
         }
 
         [Test]
@@ -32,18 +33,105 @@ namespace SimpleFeatureToggle.Tests
         {
             _fileReader
                 .Setup(x => x.ReadToEnd("test.json"))
-                .Returns("{\"test\": \"true\"}");
+                .Returns("{\"test\": 1}");
 
-            var simpleFeatureToggle = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
             {
                 Filename = "test.json"
             }, _fileReader.Object);
 
-            var result = simpleFeatureToggle.GetFeatures().ToArray();
+            var result = sft.GetFeatures().ToArray();
 
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("test", result[0].Name);
             Assert.IsTrue(result[0].Enabled);
+        }
+
+        [Test]
+        public void GetFeatures_When_FileLoaderRaisesException_ShouldReturn_EmptyCollectionOf_Features()
+        {
+            _fileReader
+                .Setup(x => x.ReadToEnd("test.json"))
+                .Throws(new Exception());
+
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            {
+                Filename = "test.json"
+            }, _fileReader.Object);
+
+            var result = sft.GetFeatures().ToArray();
+
+            Assert.AreEqual(0, result.Length);
+        }
+
+        [Test]
+        public void Find_ShouldReturn_Feature()
+        {
+            _fileReader
+                .Setup(x => x.ReadToEnd("test.json"))
+                .Returns("{\"MY_FEATURE\": 1}");
+
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            {
+                Filename = "test.json"
+            }, _fileReader.Object);
+
+            var result = sft.Find("MY_FEATURE");
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Find_When_FeatureDoesNotExist_ShouldReturn_Null()
+        {
+            _fileReader
+                .Setup(x => x.ReadToEnd("test.json"))
+                .Returns("{\"MY_FEATURE\": 1}");
+
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            {
+                Filename = "test.json"
+            }, _fileReader.Object);
+
+            var result = sft.Find("NOT_A_FEATURE");
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void IsEnabled_ShouldReturn_FeatureEnabled()
+        {
+            _fileReader
+                .Setup(x => x.ReadToEnd("test.json"))
+                .Returns("{\"MY_FEATURE\": 1}");
+
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            {
+                Filename = "test.json"
+            }, _fileReader.Object);
+
+            var result = sft.IsEnabled("MY_FEATURE");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsEnabled_When_FeatureNotFound_ShouldReturn_DefaultValue(bool defaultValue)
+        {
+            _fileReader
+                .Setup(x => x.ReadToEnd("test.json"))
+                .Returns("{\"MY_FEATURE\": " + Convert.ToInt32(defaultValue) + "}");
+
+            var sft = new SimpleFeatureToggle(new SimpleFeatureToggleConfiguration
+            {
+                Filename = "test.json"
+            }, _fileReader.Object);
+
+            var result = sft.IsEnabled("MY_FEATURE", defaultValue);
+
+            Assert.AreEqual(defaultValue, result);
         }
     }
 }
--- src/SimpleFeatureToggle/SimpleFeatureToggle.cs
+++ src/SimpleFeatureToggle/SimpleFeatureToggle.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Newtonsoft.Json;
 
@@ -22,10 +24,30 @@ namespace SimpleFeatureToggle
 
         public IEnumerable<Feature> GetFeatures()
         {
-            var content = _fileReader.ReadToEnd(_configuration.Filename);
-            var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+            try
+            {
+                var content = _fileReader.ReadToEnd(_configuration.Filename);
+                var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
 
-            return dict.Select(Feature.Parse);
+                return dict.Select(Feature.Parse);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("Failed to get features: {0}", ex);
+            }
+            return Enumerable.Empty<Feature>();
+        }
+
+        public Feature Find(string name)
+        {
+            return GetFeatures().FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool IsEnabled(string name, bool defaultValue = false)
+        {
+            var feature = Find(name);
+
+            return feature == null ? defaultValue : feature.Enabled;
         }
     }
 }
